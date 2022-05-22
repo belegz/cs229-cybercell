94032ecbaf00781e1682292209f75ae134fb178b
jessychen613 <83385232+jessychen613@users.noreply.github.com>
2022-05-22 15:19:49 -0700

    Add files via upload

--- Console.py
+++ Console.py
@@ -9,13 +9,13 @@ savepath = "command/Test/command.abstr"
 savefolder = "command"
 # os.environ['PYTHONHASHSEED'] = '0'
 
-status = 0 #0:idle 1: listening 2: action 
-MAX_CYCLE_NUM = 10 # when it reach 10 cycles, the robot quits
+status = 0 #0:human input 1: communicate 2: output
+motion_dic = {0:'go',1:'bring',2:'put',3:'find',4:'move',5:'search',6:'take',7:'think',8:'drop',9:'remove',10:'switch',11:'grab',12:'want',13:'let',14:'look',15:'control',16:'release',17:'need',18:'listen',19:'be',20:'turn',21:'',}
 
 def msg_pre_procss(message):
 #    message = message + '\n'
     out = message.translate(str.maketrans('', '', string.punctuation))
-    print(out)
+
     test_doc_str = {}
     test_doc_str['1'] = out
 
@@ -35,22 +35,21 @@ def msg_analysis(message):
         for j in range (len(list_msg)):
             new_msg = new_msg + list_msg[j] + ' '
 
-    new_msg = new_msg + '? (yes/no)' + '\n'
+    new_msg = new_msg + '?' + '\n'
     return new_msg
 
 if __name__ == '__main__':
 
-    print("Welcome to Cyber-Cell! This robot will help you to analysis your command. ")
     while True:
         #exact the key words
         if status == 0:
-            message = input("Cyber-Cell:\nUser, please input your command: \n")
+            message = input("Robot:\nHuman, please input your command: \n")
 
             message,test_doc_str = msg_pre_procss(message)
 
 #            print("test_doc_str",test_doc_str)
             np.savetxt(savepath,np.asarray([message]),fmt='%s')
-            status = 2
+
             keywords = predict_command(savefolder)
 #            print("keywords",keywords)
             new_msg = msg_analysis(keywords)
@@ -62,21 +61,3 @@ if __name__ == '__main__':
         elif status == 1:
             #communication step
             print("msg_feedback",msg_feedback)
-            if(msg_feedback.lower=="yes"):
-               status = 0 
-            else:
-                status = 2
-                action_words = input("Cyber-Cell:\nPlease provide a key word in your command: \n")
-                for i in range(MAX_CYCLE_NUM):
-                    # np.savetxt(savepath,np.asarray([message]),fmt='%s')
-                    # TODO: implement what we want to do with action words 
-                    # add action word to dictionary
-                    keywords = predict_command(savefolder)
-                    new_msg = msg_analysis(keywords)
-                    # get good result
-                    if(msg_feedback.lower=="yes"):
-                        status = 0
-                        break
-                print("Sorry, We cannot analysis your command successfully. Please try another command. ")
-                status = 0
-
