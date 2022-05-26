152411217d9e5d8db50ba75c78756399ac6d6d0b
jessychen613 <83385232+jessychen613@users.noreply.github.com>
2022-05-25 18:57:55 -0700

    Add files via upload

--- Console.py
+++ Console.py
@@ -4,24 +4,65 @@ import string
 
 import os
 from Predict import predict_command
+from com_tr import *
+import json
 
-savepath = "command/Test/command.abstr"
+#savepath_full = "command/Test/command.abstr"
+savepath = "command/Test/"
 savefolder = "command"
+temp_msg = 'never mind; never mind'
 # os.environ['PYTHONHASHSEED'] = '0'
 
 status = 0 #0:human input 1: communicate 2: output
+dic_path = './com_tr/myDictionary.pkl'
 motion_dic = {0:'go',1:'bring',2:'put',3:'find',4:'move',5:'search',6:'take',7:'think',8:'drop',9:'remove',10:'switch',11:'grab',12:'want',13:'let',14:'look',15:'control',16:'release',17:'need',18:'listen',19:'be',20:'turn',21:'',}
+
+def command_detect(message,dic_path,path):
+
+#    print("message",message)
+    tf = open("./com_tr/command_dictionary", "r")
+    word_dictionary = json.load(tf)
+#    print("word_dictionary",word_dictionary)
+
+    word_dict = {}
+
+    for key, value in word_dictionary.items():
+        word_dict[int(key)] = value
+
+#    print("word_dict",word_dict)
+    matrix = transform_text(message, word_dict)
+
+#    print("matrix",matrix)
+    tf = open("./com_tr/com_model", "r")
+    com_model = json.load(tf)
+    prediction = predict_from_naive_bayes_model(com_model, matrix,training=False)
+
+    print("prediction",prediction)
+
+#    new_msg = []
+    for i in range(len(prediction)):
+        if prediction[i] > 0:
+            print(message[i])
+#            new_msg.append(message[i]) 
+            print("path",path)
+            savepath = path + 'command' + str(i) + '.abstr'
+            np.savetxt(savepath,np.asarray([message[i]]),fmt='%s')
+            savepath2 = path + 'command' + str(i) + '.uncontr'
+            np.savetxt(savepath2,np.asarray([temp_msg]),fmt='%s')
 
 def msg_pre_procss(message):
 #    message = message + '\n'
-    out = message.translate(str.maketrans('', '', string.punctuation))
 
-    test_doc_str = {}
-    test_doc_str['1'] = out
+    msg = message.split('.')
+#    print("msg",len(msg),msg)
+    msg_list = []
+    for i in range(len(msg)-1):
+        new_msg = msg[i].translate(str.maketrans('', '', string.punctuation))
+#        out=out.split(' ')
+#        print("new_msg",new_msg)
+        msg_list.append(new_msg)
 
-    out=out.split(' ')
-
-    return out, test_doc_str
+    return msg_list
 
 def msg_analysis(message):
 #    message = message + '\n'
@@ -41,17 +82,20 @@ def msg_analysis(message):
 if __name__ == '__main__':
 
     while True:
+
         #exact the key words
         if status == 0:
             message = input("Robot:\nHuman, please input your command: \n")
 
-            message,test_doc_str = msg_pre_procss(message)
+            message = msg_pre_procss(message)
+            new_msg = command_detect(message,dic_path,savepath)
+#            print(test)
 
 #            print("test_doc_str",test_doc_str)
-            np.savetxt(savepath,np.asarray([message]),fmt='%s')
+#            np.savetxt(savepath,np.asarray([message]),fmt='%s')
 
             keywords = predict_command(savefolder)
-#            print("keywords",keywords)
+            print("keywords",keywords)
             new_msg = msg_analysis(keywords)
 
             msg_feedback = input(new_msg)
@@ -61,3 +105,4 @@ if __name__ == '__main__':
         elif status == 1:
             #communication step
             print("msg_feedback",msg_feedback)
+
