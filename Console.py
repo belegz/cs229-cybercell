25d2cf41111eebfedf19aed8a0c17cc139441815
rhit-zhangt4 <zhangtiantian1999@gmail.com>
2022-05-28 11:04:48 -0500

    Update for Console communication

--- Console.py
+++ Console.py
@@ -6,11 +6,14 @@ import os
 from Predict import predict_command
 from com_tr import *
 import json
+from get_branch import *
 
 #savepath_full = "command/Test/command.abstr"
 savepath = "command/Test/"
 savefolder = "command"
 temp_msg = 'never mind; never mind'
+current_message = ""
+MAX_CYCLE_NUM = 10 # when it reach 10 cycles, the robot quits
 # os.environ['PYTHONHASHSEED'] = '0'
 
 status = 0 #0:human input 1: communicate 2: output
@@ -40,19 +43,15 @@ def command_detect(message,dic_path,path):
     print("prediction",prediction)
 
 #    new_msg = []
-    com_list = []
     for i in range(len(prediction)):
         if prediction[i] > 0:
             print(message[i])
 #            new_msg.append(message[i]) 
-#            print("path",path)
+            print("path",path)
             savepath = path + 'command' + str(i) + '.abstr'
             np.savetxt(savepath,np.asarray([message[i]]),fmt='%s')
             savepath2 = path + 'command' + str(i) + '.uncontr'
             np.savetxt(savepath2,np.asarray([temp_msg]),fmt='%s')
-            com_list.append('command' + str(i))
-
-    return com_list
 
 def msg_pre_procss(message):
 #    message = message + '\n'
@@ -73,34 +72,35 @@ def msg_analysis(message):
 #    print("message",len(message),message)
 
     new_msg = 'Robot:\nDo you mean:'
-#    print("len(message)",len(message))
-    for k in range(len(message)):
-        msg_item = message[k]
-        for i in range(len(msg_item)):
-#            print("len(msg_item)",len(msg_item))
-            list_msg = msg_item[i]
-            for j in range (len(list_msg)):
-                new_msg = new_msg + list_msg[j] + ' '
 
-    new_msg = new_msg + '?' + '\n'
+    for i in range(len(message)):
+#        print("len(message[i])",len(message[i]))
+        list_msg = message[i]
+        for j in range (len(list_msg)):
+            new_msg = new_msg + list_msg[j] + ' '
+
+    new_msg = new_msg + '? (yes/no)' + '\n'
     return new_msg
 
 if __name__ == '__main__':
 
     while True:
-
         #exact the key words
         if status == 0:
-            message = input("Robot:\nHuman, please input your command: \n")
-
+            message = input("User, please input your command: \n")
+            current_message = str(message)
+            print(current_message)
+            if message.lower()=="exit":
+                print("Goodbye User, until next time.")
+                break
             message = msg_pre_procss(message)
-            com_list = command_detect(message,dic_path,savepath)
-            print("com_list",com_list)
+            new_msg = command_detect(message,dic_path,savepath)
+#            print(test)
 
 #            print("test_doc_str",test_doc_str)
 #            np.savetxt(savepath,np.asarray([message]),fmt='%s')
 
-            keywords = predict_command(savefolder,com_list)
+            keywords = predict_command(savefolder)
             print("keywords",keywords)
             new_msg = msg_analysis(keywords)
 
@@ -110,5 +110,32 @@ if __name__ == '__main__':
 #            break
         elif status == 1:
             #communication step
-            print("msg_feedback",msg_feedback)
+            # print("msg_feedback",msg_feedback.lower())
+            if(msg_feedback.lower()=="yes"):
+                current_message = ""
+                status = 0
+            else:
+                # print("Start communication")
+                status = 2
+                for i in range(MAX_CYCLE_NUM):
+                    action_words = input("Please provide a few action key words in your command, separate with space if you have more than one: \n")
+                    if action_words.lower()=="exit":
+                        print("Goodbye User, until next time.")
+                        exit()
+                    print("Get Key words: "+action_words)
+                    print("Message: "+current_message)
+                    action_word_list = action_words.split(" ")
+                    for a_word in action_word_list:
+                        print("TODO")
+                    new_msg = msg_analysis(keywords)
+                    msg_feedback = input(new_msg)
+                    # get good result
+                    if(msg_feedback.lower()=="yes"):
+                        status = 0
+                        current_message = ""
+                        break
+                    if(i == MAX_CYCLE_NUM - 1):
+                        current_message = ""
+                        print("Sorry, We cannot analysis your command successfully. Please try another command. ")
+                status = 0
 
--- data/Marujo2012/CorpusAndCrowdsourcingAnnotations/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-baseDir="train"
-
-for doc in `ls $baseDir/*.txt`
-do
-name=`basename $doc .txt`;
-head -1 $doc > $baseDir"/"$name"-justTitle.txt";
-done
